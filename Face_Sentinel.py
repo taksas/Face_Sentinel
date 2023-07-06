1be93c5f7b3d89193f4590202666dd412c848ddf
taksas <takuminakayama4869@gmail.com>
2023-07-06 18:56:51 +0900

    Lock functions

--- Face_Sentinel.py
+++ Face_Sentinel.py
@@ -2,12 +2,12 @@ from pynput.keyboard import Listener
 import time
 import threading
 import customtkinter
-
+import subprocess
 
 # --- Global Variables ---
 interval = 0
-debugging = True
-limit = 10   # It may be changed by GUI input
+debugging = False
+limit = 3   # It may be changed by GUI input
 # ------------------------
 
 
@@ -24,13 +24,37 @@ def interval_countup():
     while True:
         interval += 1
         time.sleep(1)
-        if(debugging):
-            print(interval)
+        if(debugging): print(interval)
 
 
 def onPress(key):   # keylog listener
     global interval
     interval = 0
+
+
+def interval_observe():   # keyboard input interval observer
+    global interval
+    global limit
+    while True:
+        time.sleep(1)
+        if(interval > limit):
+            if(debugging): print("Limit exceeded.")
+            face_check()
+
+
+
+def face_check():
+    log_off()
+
+
+def log_off():
+    global app_exit
+    if(debugging) : print("Log Off Function Triggered.")
+    else:
+        subprocess.check_output('rundll32.exe user32.dll,LockWorkStation', shell=True)
+    app.quit()
+
+
 
 class App(customtkinter.CTk):   # CustomTKinter (GUI) Class
     def __init__(self):
@@ -45,12 +69,12 @@ class App(customtkinter.CTk):   # CustomTKinter (GUI) Class
         global limit
         self.textbox = customtkinter.CTkEntry(master=self, placeholder_text="Interval Limit(sec)", width=150, font=self.fonts)
         self.textbox.place(x=0, y=0)
-        self.button = customtkinter.CTkButton(master=self, text="Apply", width=80, command=self.apply_button_function, font=self.fonts)
-        self.button.place(x=150, y=0)
+        self.button = customtkinter.CTkButton(master=self, text="Apply", width=70, command=self.apply_button_function, font=self.fonts)
+        self.button.place(x=155, y=0)
 
         self.limit_text1 = customtkinter.CTkLabel(master=self, text="Limit: ", font=self.fonts)
         self.limit_text1.place(x=250, y=0)
-        self.limit_text2 = customtkinter.CTkLabel(master=self, text="10", font=self.fonts)
+        self.limit_text2 = customtkinter.CTkLabel(master=self, text=str(limit), font=self.fonts)
         self.limit_text2.place(x=300, y=0)
     
     def apply_button_function(self):
@@ -64,8 +88,8 @@ class App(customtkinter.CTk):   # CustomTKinter (GUI) Class
 
 
 
-
 if __name__ == "__main__":
+    global app
     debugging_thread = threading.Thread(target=Debugging_Point)   # debugging point
     debugging_thread.setDaemon(True)
     debugging_thread.start()
@@ -78,6 +102,10 @@ if __name__ == "__main__":
     interval_counter= threading.Thread(target=interval_countup)   # interval count up function
     interval_counter.setDaemon(True)
     interval_counter.start()
+
+    interval_observer= threading.Thread(target=interval_observe)   # keyboard input interval observe function
+    interval_observer.setDaemon(True)
+    interval_observer.start()
     
     app = App()
     app.mainloop()
